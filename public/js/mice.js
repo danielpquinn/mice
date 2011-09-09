bcdb0b075b92b06e597149fff52c73b3de8aa250
Dan Quinn <dan@danquinndesign.com>
2011-09-09 01:27:43 -0700

    Unique IDs and events are happnin'

--- public/js/mice.js
+++ public/js/mice.js
@@ -1,83 +1,72 @@
-var socket = io.connect('http://localhost');
-socket.on('news', function (data) {
-  console.log(data);
-  socket.emit('my other event', { my: 'data' });
+var socket = io.connect('http://localhost'),
+  id;
+socket.on('player connect', function(data) {
+    console.log('player connected');
+    console.log(data.players);
 });
-
+socket.on('player disconnect', function(data) {
+    console.log('player disconnected');
+    console.log(data.players);
+});
+socket.on('player keydown', function(data) {
+    console.log(data.id + ' pressed ' + data.keypressed);
+});
 jQuery(document).ready(function() {
-  
-  scene = mice.createScene();
-  
+    scene = mice.createScene();
 });
-
 var mice = {};
 mice.createScene = function() {
+    var container, camera, scene, webglRenderer;
+    var windowHalfX = window.innerWidth / 2;
+    var windowHalfY = window.innerHeight / 2;
+    var mesh, mesh_R_eye, mesh_L_eye;
 
-  // set the scene size
-	var WIDTH = 400,
-	    HEIGHT = 300;
-	
-	// set some camera attributes
-	var VIEW_ANGLE = 45,
-	    ASPECT = WIDTH / HEIGHT,
-	    NEAR = 0.1,
-	    FAR = 10000;
-	
-	// get the DOM element to attach to
-	// - assume we've got jQuery to hand
-	var $container = jQuery('#container');
-	
-	// create a WebGL renderer, camera
-	// and a scene
-	var renderer = new THREE.WebGLRenderer();
-	var camera = new THREE.Camera(  VIEW_ANGLE,
-	                                ASPECT,
-	                                NEAR,
-	                                FAR  );
-	var scene = new THREE.Scene();
-	
-	// the camera starts at 0,0,0 so pull it back
-	camera.position.z = 300;
-	
-	// start the renderer
-	renderer.setSize(WIDTH, HEIGHT);
-	
-	// attach the render-supplied DOM element
-	$container.append(renderer.domElement);
-	
-	// create a point light
-	var pointLight = new THREE.PointLight( 0xFFFFFF );
-	
-	// set its position
-	pointLight.position.x = 10;
-	pointLight.position.y = 50;
-	pointLight.position.z = 130;
-	
-	// add to the scene
-	scene.addLight(pointLight);
-	
-	// create the sphere's material
-	var sphereMaterial = new THREE.MeshLambertMaterial(
-	{
-	    color: 0xCC0000
-	});
-	
-	// set up the sphere vars
-	var radius = 50, segments = 16, rings = 16;
-
-	// create a new mesh with sphere geometry -
-	// we will cover the sphereMaterial next!
-	var sphere = new THREE.Mesh(
-	   new THREE.SphereGeometry(radius, segments, rings),
-	   sphereMaterial);
-	
-  scene.addChild(sphere);
-	
-	// draw!
-	renderer.render(scene, camera);
-
+    function init() {
+        // Setup the container that the simulation will happen in
+        container = document.getElementById('container');
+        camera = new THREE.Camera(70, window.innerWidth / window.innerHeight, 1, 1000);
+        camera.position.y = 150;
+        camera.position.z = 500;
+        camera.target.position.y = 150;
+        // Setup scene
+        scene = new THREE.Scene();
+        // Setup Group
+        group = new THREE.Object3D();
+        // create a point light
+        directionalLight = new THREE.DirectionalLight(0xffffff);
+        directionalLight.position.x = 200;
+        directionalLight.position.y = 400;
+        directionalLight.position.z = 200;
+        directionalLight.position.normalize();
+        scene.addLight(directionalLight);
+    }
+    function initRenderer() {
+        webglRenderer = new THREE.WebGLRenderer();
+        webglRenderer.setSize(window.innerWidth, window.innerHeight);
+        container.appendChild(webglRenderer.domElement);
+    }
+    init();
+    initRenderer();
+    webglRenderer.render(scene, camera);
 }
 
-mice.newPlayer = function() {
-	
-}
+$(document).keydown(function(e) {
+    switch(e.keyCode) {
+      case 37:
+      e.preventDefault();
+        socket.emit('keydown', { keypressed: 'left' });
+        break;
+      case 38:
+      e.preventDefault();
+        socket.emit('keydown', { keypressed: 'up' });
+        break;
+      case 39:
+      e.preventDefault();
+        socket.emit('keydown', { keypressed: 'right' });
+        break;
+      case 40:
+      e.preventDefault();
+        socket.emit('keydown', { keypressed: 'down' });
+        break;
+    }
+});
