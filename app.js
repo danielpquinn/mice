57ad34deb7f7c812b89f367a7048408ebafc0332
Dan Quinn <dan@danquinndesign.com>
2011-09-11 11:57:22 -0700

    added player objects to server and client. added server/client sync events

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app = express.createServer(),
     io = require('socket.io').listen(app),
-    players = [];
+    players = [],
+    tid;
 app.listen(8000); // Configuration
 app.configure(function() {
     app.set('views', __dirname + '/views');
@@ -26,21 +27,70 @@ app.get('/', function(req, res) {
     });
 });
 io.sockets.on('connection', function(socket) {
-    id = socket.id;
-    players.push(id);
+
+    var id = socket.id;
+    newPlayer = createPlayer(id);
+    players.push(newPlayer);
     io.sockets.emit('player connect', {
-        id: id
+        id: id,
+        players: players
     });
+    
     socket.on('keydown', function(data) {
-        io.sockets.emit('player keydown', {
-            keypressed: data.keypressed,
-            id: socket.id
-        });
+      updatePlayers(id, data);
     });
-    socket.on('disconnect', function(id) {
-        players.splice(players.indexOf(id), 1);
+    
+    socket.on('disconnect', function() {
         io.sockets.emit('player disconnect', {
-            id: id
+            id: id,
+            players: players
         });
+        for( var i = 0; i < players.length; i++) {
+          if(players[i].id === id) {
+            players.splice(i, 1);
+          }
+        }
     });
+    
 });
+
+// Do the timer!
+
+gameLoop = function() {
+  io.sockets.emit('game update', { players: players });
+  tid = setTimeout(function() {gameLoop()}, 2000);
+}
+
+gameLoop();
+
+updatePlayers = function(id, data) {
+  console.log(data.id + ' pressed ' + data.keypressed);
+  for( var i = 0; i < players.length; i++) {
+    if(players[i].id === id) {
+      switch(data.keypressed) {
+        case 'left':
+          players[i].x -= 10;
+          break;
+        case 'right':
+          players[i].x += 10;
+          break;
+        case 'up':
+          players[i].z += 10;
+          break;
+        case 'down':
+          players[i].z -= 10;
+          break;
+      }
+    }
+  }
+}
+
+createPlayer = function(id) {
+  player = {
+    x: 0,
+    y: 0,
+    z: 0,
+    id: id
+  };
+  return player;
+}
--- public/js/mice.js
+++ public/js/mice.js
@@ -1,15 +1,41 @@
 var socket = io.connect('http://localhost'),
     players = [];
-socket.on('player connect', function(data) {
+socket.on('player connect', function(data) { 
     console.log(data.id + ' connected');
-    mice.sphere = mice.createSphere();
-    mice.scene.addChild(mice.sphere);
+    for(var i = 0; i < data.players.length; i++) {
+      if(!players[i]) {
+        var newSphere = mice.createSphere();
+        var newPlayer = {
+          id: data.players[i].id,
+          body: newSphere
+        }
+        players.push(newPlayer);
+        newPlayer.body.position.x = data.players[i].x;
+        newPlayer.body.position.y = data.players[i].y;
+        newPlayer.body.position.z = data.players[i].z;
+        mice.scene.addChild(newPlayer.body);
+      }
+    }
+
 });
 socket.on('player disconnect', function(data) {
     console.log(data.id + ' disconnected');
+    for( var i = 0; i < players.length; i++) {
+      if(players[i].id === data.id) {
+        mice.scene.removeChild(players[i].body);
+        players.splice(i, 1);
+      }
+    }
 });
-socket.on('player keydown', function(data) {
-    console.log('player ' + data.id + ' pressed ' + data.keypressed);
+socket.on('game update', function(data) {
+    console.log('game updated');
+    console.log(data.players);
+    console.log(players);
+    for(var i = 0; i < data.players.length; i++) {
+      players[i].body.position.x = data.players[i].x;
+      players[i].body.position.y = data.players[i].y;
+      players[i].body.position.z = data.players[i].z;
+    }
 });
 jQuery(document).ready(function() {
     mice.init();
@@ -17,11 +43,19 @@ jQuery(document).ready(function() {
 var mice = {};
 mice.init = function() {
     mice.container = document.getElementById('container');
-    mice.camera = new THREE.Camera(70, window.innerWidth / window.innerHeight, 1, 1000);
+    mice.camera = new THREE.FirstPersonCamera( {
+  
+  		fov: 50, aspect: window.innerWidth / window.innerHeight, near: 1, far: 20000,
+  		constrainVertical: true, verticalMin: 1.1, verticalMax: 2.2,
+  		movementSpeed: 1000, lookSpeed: 0.125, noFly: false, lookVertical: true, autoForward: false
+  
+  	} );
+  	mice.camera.target.position.z = - 100;
     mice.scene = new THREE.Scene();
+  	mice.scene.fog = new THREE.FogExp2( 0xffffff, 0.00015 );
     mice.webglRenderer = new THREE.WebGLRenderer();
     mice.createScene();
-    mice.tid = setTimeout(function(){mice.loop()}, 100);
+    mice.animate();
 } // Setup scene
 mice.createScene = function() {
     mice.camera = new THREE.Camera(70, window.innerWidth / window.innerHeight, 1, 1000);
@@ -116,9 +150,7 @@ $(document).keyup(function(e) {
     }
 });
 
-mice.loop = function() {
-    if(mice.leftPressed){mice.camera.position.x += 100}
-    if(mice.rightPressed){mice.camera.position.x -= 100}
-    mice.webglRenderer.render(mice.scene, mice.camera);
-    mice.tid = setTimeout(function(){mice.loop()}, 100);
+mice.animate = function() {
+  requestAnimationFrame( mice.animate );
+  mice.webglRenderer.render(mice.scene, mice.camera);
 }
