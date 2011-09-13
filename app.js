0a485f8ff036276aa3e4d42cc680bc8e9dd9576b
Dan Quinn <dan@danquinndesign.com>
2011-09-12 23:36:25 -0700

    Namespaced everything, handling game loop on server side

--- app.js
+++ app.js
@@ -1,96 +1,143 @@
-var express = require('express'),
-    app = express.createServer(),
-    io = require('socket.io').listen(app),
-    players = [],
-    tid;
-app.listen(8000); // Configuration
-app.configure(function() {
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
-});
-app.configure('development', function() {
-    app.use(express.errorHandler({
-        dumpExceptions: true,
-        showStack: true
-    }));
-});
-app.configure('production', function() {
-    app.use(express.errorHandler());
-}); // Routes
-app.get('/', function(req, res) {
-    res.render('index', {
-        title: 'Mice'
-    });
-});
-io.sockets.on('connection', function(socket) {
-
-    var id = socket.id;
-    newPlayer = createPlayer(id);
-    players.push(newPlayer);
-    io.sockets.emit('player connect', {
-        id: id,
-        players: players
-    });
-    
-    socket.on('keydown', function(data) {
-      updatePlayers(id, data);
-    });
-    
-    socket.on('disconnect', function() {
-        io.sockets.emit('player disconnect', {
-            id: id,
-            players: players
+// create app Object
+var app = {
+    players: [],
+    express: 0,
+    server: 0,
+    io: 0,
+    tid: 0
+};
+app.init = function() { // init app properties    
+    this.players = [];
+    this.express = require('express');
+    this.server = this.express.createServer();
+    this.io = require('socket.io').listen(this.server); // create local references to properties
+    var express = this.express,
+        server = this.server,
+        io = this.io,
+        tid = this.tid; // listen on 8000
+    server.listen(8000); // configure server
+    server.configure(function() {
+        server.set('views', __dirname + '/views');
+        server.set('view engine', 'jade');
+        server.use(express.bodyParser());
+        server.use(express.methodOverride());
+        server.use(server.router);
+        server.use(express.static(__dirname + '/public'));
+    }); // add development configuration
+    server.configure('development', function() {
+        server.use(express.errorHandler({
+            dumpExceptions: true,
+            showStack: true
+        }));
+    }); // add production configuration
+    server.configure('production', function() {
+        server.use(express.errorHandler());
+    }); // set up routes
+    server.get('/', function(req, res) {
+        res.render('index', {
+            title: 'Mice'
         });
-        for( var i = 0; i < players.length; i++) {
-          if(players[i].id === id) {
-            players.splice(i, 1);
-          }
-        }
+    }); // add server event listeners
+    io.sockets.on('connection', function(socket) { // get unique player id
+        var id = socket.id;
+        newPlayer = app.createPlayer(id);
+        app.players.push(newPlayer);
+        io.sockets.emit('player connect', {
+            id: id,
+            players: app.players
+        });
+        socket.on('keydown', function(data) {
+            app.onKeyDown(id, data);
+            app.updatePlayers();
+            io.sockets.emit('game update', {
+                players: app.players
+            });
+        });
+        socket.on('keyup', function(data) {
+            app.onKeyUp(id, data);
+        });
+        socket.on('disconnect', function() {
+            io.sockets.emit('player disconnect', {
+                id: id,
+                players: app.players
+            });
+            for (var i = 0, max = app.players.length; i < max; i++) {
+                if (app.players[i].id === id) {
+                    app.players.splice(i, 1);
+                }
+            }
+        });
     });
-    
-});
-
-// Do the timer!
-
-gameLoop = function() {
-  io.sockets.emit('game update', { players: players });
-  tid = setTimeout(function() {gameLoop()}, 2000);
 }
-
-gameLoop();
-
-updatePlayers = function(id, data) {
-  console.log(data.id + ' pressed ' + data.keypressed);
-  for( var i = 0; i < players.length; i++) {
-    if(players[i].id === id) {
-      switch(data.keypressed) {
-        case 'left':
-          players[i].x -= 10;
-          break;
-        case 'right':
-          players[i].x += 10;
-          break;
-        case 'up':
-          players[i].z += 10;
-          break;
-        case 'down':
-          players[i].z -= 10;
-          break;
-      }
+app.createPlayer = function(id) {
+    player = {
+        x: 0,
+        y: 0,
+        z: 0,
+        leftPressed: false,
+        rightPressed: false,
+        upPressed: false,
+        downPressed: false,
+        id: id
+    };
+    return player;
+}
+app.onKeyDown = function(id, data) {
+    console.log(id + ' pressed ' + data.keypressed);
+    for (var i = 0, max = app.players.length; i < max; i++) {
+        if (app.players[i].id === id) {
+            switch (data.keypressed) {
+            case 'left':
+                app.players[i].leftPressed = true;
+                break;
+            case 'right':
+                app.players[i].rightPressed = true;
+                break;
+            case 'up':
+                app.players[i].upPressed = true;
+                break;
+            case 'down':
+                app.players[i].downPressed = true;
+                break;
+            }
+        }
     }
-  }
 }
-
-createPlayer = function(id) {
-  player = {
-    x: 0,
-    y: 0,
-    z: 0,
-    id: id
-  };
-  return player;
+app.onKeyUp = function(id, data) {
+    console.log(id + ' lifted ' + data.keylifted);
+    for (var i = 0, max = app.players.length; i < max; i++) {
+        if (app.players[i].id === id) {
+            switch (data.keylifted) {
+            case 'left':
+                app.players[i].leftPressed = false;
+                break;
+            case 'right':
+                app.players[i].rightPressed = false;
+                break;
+            case 'up':
+                app.players[i].upPressed = false;
+                break;
+            case 'down':
+                app.players[i].downPressed = false;
+                break;
+            }
+        }
+    }
 }
+app.updatePlayers = function() {
+    for (var i = 0, max = app.players.length; i < max; i++) {
+        if (app.players[i].leftPressed) {
+            app.players[i].x -= 10;
+        }
+        if (app.players[i].rightPressed) {
+            app.players[i].x += 10;
+        }
+        if (app.players[i].upPressed) {
+            app.players[i].y += 10;
+        }
+        if (app.players[i].downPressed) {
+            app.players[i].y -= 10;
+        }
+    }
+}
+app.init();
--- public/js/mice.js
+++ public/js/mice.js
@@ -1,117 +1,116 @@
-var socket = io.connect('http://localhost'),
-    players = [];
-socket.on('player connect', function(data) { 
-    console.log(data.id + ' connected');
-    for(var i = 0; i < data.players.length; i++) {
-      if(!players[i]) {
-        var newSphere = mice.createSphere();
-        var newPlayer = {
-          id: data.players[i].id,
-          body: newSphere
-        }
-        players.push(newPlayer);
-        newPlayer.body.position.x = data.players[i].x;
-        newPlayer.body.position.y = data.players[i].y;
-        newPlayer.body.position.z = data.players[i].z;
-        mice.scene.addChild(newPlayer.body);
-      }
-    }
-
-});
-socket.on('player disconnect', function(data) {
-    console.log(data.id + ' disconnected');
-    for( var i = 0; i < players.length; i++) {
-      if(players[i].id === data.id) {
-        mice.scene.removeChild(players[i].body);
-        players.splice(i, 1);
-      }
-    }
-});
-socket.on('game update', function(data) {
-    console.log('game updated');
-    console.log(data.players);
-    console.log(players);
-    for(var i = 0; i < data.players.length; i++) {
-      players[i].body.position.x = data.players[i].x;
-      players[i].body.position.y = data.players[i].y;
-      players[i].body.position.z = data.players[i].z;
-    }
-});
-jQuery(document).ready(function() {
-    mice.init();
-}); //mice object
-var mice = {};
+var mice = {
+    players: [],
+    socket: io.connect('http://localhost')
+};
 mice.init = function() {
-    mice.container = document.getElementById('container');
-    mice.camera = new THREE.FirstPersonCamera( {
-  
-  		fov: 50, aspect: window.innerWidth / window.innerHeight, near: 1, far: 20000,
-  		constrainVertical: true, verticalMin: 1.1, verticalMax: 2.2,
-  		movementSpeed: 1000, lookSpeed: 0.125, noFly: false, lookVertical: true, autoForward: false
-  
-  	} );
-  	mice.camera.target.position.z = - 100;
-    mice.scene = new THREE.Scene();
-  	mice.scene.fog = new THREE.FogExp2( 0xffffff, 0.00015 );
-    mice.webglRenderer = new THREE.WebGLRenderer();
-    mice.createScene();
-    mice.animate();
+    var socket = this.socket,
+        players = this.players;
+    socket.on('player connect', function(data) {
+        console.log(data.id + ' connected');
+        for (var i = 0, max = data.players.length; i < max; i++) {
+            if (!players[i]) {
+                var newSphere = mice.createSphere();
+                var newPlayer = {
+                    id: data.players[i].id,
+                    body: newSphere
+                }
+                players.push(newPlayer);
+                newPlayer.body.position.x = data.players[i].x;
+                newPlayer.body.position.y = data.players[i].y;
+                newPlayer.body.position.z = data.players[i].z;
+                mice.scene.addChild(newPlayer.body);
+            }
+        }
+    });
+    socket.on('player disconnect', function(data) {
+        console.log(data.id + ' disconnected');
+        for (var i = 0, max = players.length; i < max; i++) {
+            if (players[i].id === data.id) {
+                mice.scene.removeChild(players[i].body);
+                players.splice(i, 1);
+            }
+        }
+    });
+    socket.on('game update', function(data) {
+        for (var i = 0, max = data.players.length; i < max; i++) {
+            players[i].body.position.x = data.players[i].x;
+            players[i].body.position.y = data.players[i].y;
+            players[i].body.position.z = data.players[i].z;
+        }
+    });
+    this.container = document.getElementById('container');
+    this.camera = new THREE.FirstPersonCamera({
+        fov: 50,
+        aspect: window.innerWidth / window.innerHeight,
+        near: 1,
+        far: 20000,
+        constrainVertical: true,
+        verticalMin: 1.1,
+        verticalMax: 2.2,
+        movementSpeed: 1000,
+        lookSpeed: 0.125,
+        noFly: false,
+        lookVertical: true,
+        autoForward: false
+    });
+    this.camera.target.position.z = -100;
+    this.scene = new THREE.Scene();
+    this.scene.fog = new THREE.FogExp2(0xffffff, 0.00015);
+    this.webglRenderer = new THREE.WebGLRenderer();
+    this.createScene();
+    this.animate();
 } // Setup scene
 mice.createScene = function() {
-    mice.camera = new THREE.Camera(70, window.innerWidth / window.innerHeight, 1, 1000);
-    mice.camera.position.y = 150;
-    mice.camera.position.z = 500;
-    mice.camera.target.position.y = 150; // create a point light
+    this.camera = new THREE.Camera(70, window.innerWidth / window.innerHeight, 1, 1000);
+    this.camera.position.y = 150;
+    this.camera.position.z = 500;
+    this.camera.target.position.y = 150; // create a point light
     directionalLight = new THREE.DirectionalLight(0xffffff);
     directionalLight.position.x = 200;
     directionalLight.position.y = 400;
     directionalLight.position.z = 200;
     directionalLight.position.normalize();
-    mice.scene.addLight(directionalLight);
-    mice.webglRenderer.setSize(window.innerWidth, window.innerHeight);
-    mice.container.appendChild(mice.webglRenderer.domElement);
+    this.scene.addLight(directionalLight);
+    this.webglRenderer.setSize(window.innerWidth, window.innerHeight);
+    this.container.appendChild(mice.webglRenderer.domElement);
 }
-mice.createSphere = function() {
-    // create the sphere's material
+mice.createSphere = function() { // create the sphere's material
     var sphereMaterial = new THREE.MeshLambertMaterial({
         color: 0xCC0000
-    });
-    // set up the sphere vars
+    }); // set up the sphere vars
     var radius = 50,
         segments = 16,
-        rings = 16;
-    // create a new mesh with sphere geometry -
+        rings = 16; // create a new mesh with sphere geometry -
     var sphere = new THREE.Mesh(new THREE.SphereGeometry(radius, segments, rings), sphereMaterial);
     return sphere;
-}
-//Keyboard Events!
+} //Keyboard Events!
 $(document).keydown(function(e) {
     switch (e.keyCode) {
     case 37:
         e.preventDefault();
         mice.leftPressed = true;
-        socket.emit('keydown', {
+        mice.socket.emit('keydown', {
             keypressed: 'left'
         });
         break;
     case 38:
         e.preventDefault();
         mice.upPressed = true;
-        socket.emit('keydown', {
+        mice.socket.emit('keydown', {
             keypressed: 'up'
         });
         break;
     case 39:
         e.preventDefault();
         mice.rightPressed = true;
-        socket.emit('keydown', {
+        mice.socket.emit('keydown', {
             keypressed: 'right'
         });
         break;
     case 40:
         e.preventDefault();
         mice.downPressed = true;
-        socket.emit('keydown', {
+        mice.socket.emit('keydown', {
             keypressed: 'down'
         });
         break;
@@ -122,35 +121,39 @@ $(document).keyup(function(e) {
     case 37:
         e.preventDefault();
         mice.leftPressed = false;
-        socket.emit('keyup', {
+        mice.socket.emit('keyup', {
             keylifted: 'left'
         });
         break;
     case 38:
         e.preventDefault();
         mice.upPressed = false;
-        socket.emit('keyup', {
+        mice.socket.emit('keyup', {
             keylifted: 'up'
         });
         break;
     case 39:
         e.preventDefault();
         mice.rightPressed = false;
-        socket.emit('keyup', {
+        mice.socket.emit('keyup', {
             keylifted: 'right'
         });
         break;
     case 40:
         e.preventDefault();
         mice.downPressed = false;
-        socket.emit('keyup', {
+        mice.socket.emit('keyup', {
             keylifted: 'down'
         });
         break;
     }
 });
-
 mice.animate = function() {
-  requestAnimationFrame( mice.animate );
-  mice.webglRenderer.render(mice.scene, mice.camera);
+    mice.webglRenderer.render(mice.scene, mice.camera);
+    tid = setTimeout(function() {
+        mice.animate();
+    }, 100);
 }
+jQuery(document).ready(function() {
+    mice.init();
+});
